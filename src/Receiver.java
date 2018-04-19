3846acfe18b98880f9a550a7f0f470de098a3529
Davide <davide.cattaneo.it@gmail.com>
2018-04-19 13:07:45 +0200

    Revert "Fix"
    This reverts commit 2dd2f439be2e0100af40ece52fd11793696dc3a6.

--- src/Receiver.java
+++ src/Receiver.java
@@ -594,8 +594,7 @@ public class Receiver extends Thread {
 				
 				//mess.print();
 				
-				if (!mess.type.equals("unlock") && !mess.type.equals("ok") && !mess.type.equals("send") && !mess.type.equals("missing") 
-						&& !mess.type.equals("shouldEx") && !mess.type.equals("go")) {
+				if (!mess.type.equals("unlock") && !mess.type.equals("ok") && !mess.type.equals("send") && !mess.type.equals("missing")) {
 					
 					//mess.print();
 					
